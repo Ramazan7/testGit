60ff97dd3902b9220ca3a596105194c8b0e66049
Ramazan <ramazan05.00@mail.ru>
2021-06-29 20:54:52 +0300

    first

---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by Admin on 27.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
